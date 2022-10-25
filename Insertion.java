946cf04ae885f05e6e7dd719a79758afc5887c27
Balvannanathan <113589227+balamt15@users.noreply.github.com>
2022-10-25 07:01:34 -0700

    Add files via upload

---
+++ Insertion.java
@@ -0,0 +1,19 @@
+class Insertion{
+public static void main(String args[]){
+int arr[]={20,10,50,30,60,40};
+int n =arr.length;
+for(int i=1;i<n;i++){
+	int key=arr[i];
+	int j=i-1;
+	while(j>=0 && arr[j]>key){
+		arr[j+1]=arr[j];	
+		j--;
+		}
+	arr[j+1]=key;
+	}
+for(int i=0;i<n;i++){
+	System.out.print(arr[i]+" ");
+	}
+}
+}
+	
