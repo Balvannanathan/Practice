ae0fb0e8281fdc33432378574292a3d32629c6d2
Balvannanathan <113589227+balamt15@users.noreply.github.com>
2022-10-25 06:57:21 -0700

    Add files via upload

---
+++ NonRep.java
@@ -0,0 +1,17 @@
+class NonRep{
+public static void main(String[] args){
+String str="Balvannanathan";
+char ch[]=str.toCharArray();
+for(int i=0;i<ch.length;i++){
+	int count=0;
+	for(int j=0;j<ch.length;j++){
+		if(ch[i]==ch[j]){
+			count++;
+			}
+		}
+	if(count<=1){
+		System.out.print(ch[i]+" ");
+		}
+	}
+}
+}
